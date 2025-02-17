3cbaa236b369ab41a1690e91814850a14557c2be
EnvYend Ricardo YanHua <ricardoyanhua@gmail.com>
2025-02-16 20:45:24 -0500

    init

---
+++ src/test/java/com/yanhua/app/WebSiteYanHuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanhua.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteYanHuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
